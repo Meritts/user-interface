f70772525c531528b42200adbd07e4d6d5a41a38
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2013-11-13 19:02:41 -0200

    Allowing children to every component

--- src/Meritt/UI/UIComponent.php
+++ src/Meritt/UI/UIComponent.php
@@ -3,15 +3,19 @@
 namespace Meritt\UI;
 
 use Lcobucci\ActionMapper2\Http\Request;
+use Lcobucci\DisplayObjects\Core\UIComponent as Component;
+use Lcobucci\DisplayObjects\Core\UIComponentNotFoundException;
 use Meritt\Gimme\Package;
 use Meritt\Gimme\Receiver as ReceiverInterface;
 use Meritt\Gimme\UserInterface\Receiver;
 use Meritt\Gimme\UserInterface\ReceiverHook;
 
-abstract class UIComponent extends \Lcobucci\DisplayObjects\Core\UIComponent implements
-    ReceiverInterface
+abstract class UIComponent extends Component implements ReceiverInterface
 {
-    use Receiver;
+    use Receiver {
+        requires as baseRequires;
+        calls as baseCalls;
+    }
 
     /**
      * The HTTP request
@@ -19,6 +23,30 @@ abstract class UIComponent extends \Lcobucci\DisplayObjects\Core\UIComponent imp
      * @var Request
      */
     protected static $request;
+
+    /**
+     * The list of child components
+     *
+     * @var UIComponent[]
+     */
+    protected $childNodes;
+
+    /**
+     * The parent component
+     *
+     * @var UIComponent
+     */
+    protected $parentNode;
+
+    /**
+     * @var array
+     */
+    protected $renderedChildren;
+
+    /**
+     * @var boolean
+     */
+    protected $configured;
 
     /**
      * Configures the HTTP request
@@ -41,6 +69,121 @@ abstract class UIComponent extends \Lcobucci\DisplayObjects\Core\UIComponent imp
     }
 
     /**
+     * Class constructor
+     */
+    public function __construct()
+    {
+        $this->childNodes = [];
+        $this->renderedChildren = [];
+        $this->configured = false;
+    }
+
+    /**
+     * @param UIComponent $parentNode
+     */
+    public function setParentNode(UIComponent $parentNode)
+    {
+        $this->parentNode = $parentNode;
+    }
+
+    /**
+     * @param Component $component
+     */
+    public function appendChild(Component $component)
+    {
+        $this->childNodes[] = $component;
+
+        if ($component instanceof UIComponent) {
+            $component->setParentNode($this);
+        }
+    }
+
+    /**
+     * @param Component $component
+     */
+    public function prependChild(Component $component)
+    {
+        array_unshift($this->childNodes, $component);
+
+        if ($component instanceof UIComponent) {
+            $component->setParentNode($this);
+        }
+    }
+
+    /**
+     * @param string $index
+     * @return string
+     */
+    public function renderChild($index)
+    {
+        if (!isset($this->childNodes[$index])) {
+            return '';
+        }
+
+        return $this->renderComponent($this->childNodes[$index]);
+    }
+
+    /**
+     * Renders the given component
+     *
+     * @param Component $component
+     * @return string
+     */
+    protected function renderComponent(Component $component)
+    {
+        $hash = spl_object_hash($component);
+
+        if (in_array($hash, $this->renderedChildren, true)) {
+            return '';
+        }
+
+        $this->renderedChildren[] = $hash;
+
+        return (string) $component;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function requires($resource)
+    {
+        if ($this->parentNode) {
+            return $this->parentNode->requires($resource);
+        }
+
+        return $this->baseRequires($resource);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function calls($resource, $behavior, array $parameters = array())
+    {
+        if ($this->parentNode) {
+            return $this->parentNode->calls($resource, $behavior, $parameters);
+        }
+
+        return $this->baseCalls($resource, $behavior, $parameters);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getChildrenContent()
+    {
+        $content = '';
+
+        array_walk(
+            $this->childNodes,
+            function (Component $child) use (&$content) {
+                $content .= $this->renderComponent($child);
+            }
+        );
+
+        return $content;
+    }
+
+    /**
      * Returns the string representation of the object
      *
      * @param string $class
@@ -48,14 +191,55 @@ abstract class UIComponent extends \Lcobucci\DisplayObjects\Core\UIComponent imp
      */
     public function show($class = null)
     {
-        $this->configure();
-        $this->configureRequirements();
+        $this->configureComponent($this);
+        $this->configureChildren();
 
-        $content = parent::show($class);
+        $content = $this->getComponentContent($class);
 
         return $this->getStyleTags()
                . $content
                . $this->getScriptTags();
+    }
+
+    /**
+     * Retrieves the component content
+     *
+     * @param string $class
+     * @return string
+     */
+    protected function getComponentContent($class = null)
+    {
+        try {
+            return parent::show($class);
+        } catch (UIComponentNotFoundException $exception) {
+            return $this->getChildrenContent();
+        }
+    }
+
+    /**
+     * Configures the children components
+     */
+    protected function configureChildren()
+    {
+        foreach ($this->childNodes as $child) {
+            $this->configureComponent($child);
+        }
+    }
+
+    /**
+     * Configures the given component
+     *
+     * @param UIComponent $component
+     */
+    protected function configureComponent(UIComponent $component)
+    {
+        if ($component->configured) {
+            return ;
+        }
+
+        $component->configure();
+        $component->configureRequirements();
+        $component->configured = true;
     }
 
     /**
@@ -129,13 +313,13 @@ abstract class UIComponent extends \Lcobucci\DisplayObjects\Core\UIComponent imp
     }
 
     /**
-     * @param ReceiverHook $hook
      * @return string
      */
     protected function createHookTag()
     {
         $map = [];
 
+        /** @var $hook ReceiverHook */
         foreach ($this->hooks as $hook) {
             if (!isset($map[$hook->getBehavior()])) {
                 $map[$hook->getBehavior()] = [];
@@ -144,8 +328,12 @@ abstract class UIComponent extends \Lcobucci\DisplayObjects\Core\UIComponent imp
             $map[$hook->getBehavior()][] = $hook->getParameters();
         }
 
+        $hookList = array_keys($map);
+        array_unshift($hookList, 'mcc');
+
         return sprintf(
-            '<script type="text/javascript">require([\'mcc\', \'' . $hook->getBehavior() . '\'], function(mcc){ mcc.init_behaviors(%s); });</script>',
+            '<script type="text/javascript">require(%s, function(mcc){ mcc.init_behaviors(%s); });</script>',
+            json_encode($hookList),
             json_encode($map)
         );
     }
